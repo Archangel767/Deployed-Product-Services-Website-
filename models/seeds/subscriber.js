a808a4d45f27ffc600aaa46f8fcbef66f80277d1
Mysia Santana <mysiasantana@gmail.com>
2021-11-12 14:46:58 -0700

    moved subscibers.js to seed and created subscriber.js

---
+++ models/seeds/subscriber.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const subscriberSchema = mongoose.Schema({
+    name: String,
+    email: String,
+
+});
+
+module.exports = mongoose.model('Subscriber', foodSchema);
