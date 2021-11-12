662e218a1003368852f149a8a70f862bc520f232
Mysia Santana <mysiasantana@gmail.com>
2021-11-12 13:28:24 -0700

    edited api.js

--- routes/api.js
+++ routes/api.js
@@ -1,31 +1,85 @@
 /* Define data array */
 
-const intFood = [
-
-  {
-    id: 01,
-    name: '',
-    description: '',
-    imageSrc: '',
-    width: '',
-    height: ''
-  },
-  {
-    id: 02,
-    name: '',
-    description: '',
-    imageSrc: '',
-    width: '',
-    height: ''
-  },
-  {
-    id: 03,
-    name: '',
-    description: '',
-    imageSrc: '',
-    width: '',
-    height: ''
-  },
+const intFood = [{
+        name: 'Hamburger',
+        description: 'bread, beef, cheese, salad, ketchup',
+        country: 'USA',
+        imgSrc: 'https://pixabay.com/photos/hamburger-burger-barbeque-bbq-beef-1238246/',
+        width: '300',
+        height: '300',
+    },
+    {
+        name: 'Pizza',
+        description: 'dough, tomato sauce, cheese, olives',
+        country: 'Italy',
+        imgSrc: 'https://pixabay.com/photos/pizza-italian-pasta-food-cheese-5179939/',
+        width: '300',
+        height: '300',
+    },
+    {
+        name: 'Tandoori',
+        description: 'meats, breads and vegetables',
+        imgSrc: 'https://pixabay.com/photos/india-food-indian-meal-2731817/',
+        country: 'India',
+        width: '300',
+        height: '300',
+    },
+    {
+        name: 'Sushi',
+        description: 'vinegared rice, seafood and vegetables',
+        country: 'Japan',
+        imgSrc: 'https://pixabay.com/photos/tuna-salmon-japan-seafood-fish-1957234/',
+        width: '300',
+        height: '300',
+    },
+    {
+        name: 'Dumplings',
+        description: 'flour, potatoes or bread',
+        country: 'Taiwan',
+        imgSrc: 'https://pixabay.com/photos/dumpling-taiwan-tasty-2392893/',
+        width: '300',
+        height: '300',
+    },
+    {
+        name: 'Paella',
+        description: 'white rice, shrimp and saffron',
+        country: 'Spain',
+        imgSrc: 'https://pixabay.com/photos/seafood-paella-paella-seafood-spain-6280045/',
+        width: '300',
+        height: '300',
+    },
+    {
+        name: 'Greek Salad',
+        description: 'tomatoes, cucumbers, onions',
+        country: 'Greece',
+        imgSrc: 'https://pixabay.com/photos/food-plate-greek-salad-caprese-3337621/',
+        width: '300',
+        height: '300',
+    },
+    {
+        name: 'Fried Pork',
+        description: 'thick pork chops, salt and pepper',
+        country: 'Korea',
+        imgSrc: 'https://pixabay.com/photos/pork-meat-fried-korean-food-dinner-1582916/',
+        width: '300',
+        height: '300',
+    },
+    {
+        name: 'Tacos',
+        description: 'flour tortillas, chili powder, skirt steak in pieces',
+        country: 'Mexico',
+        imgSrc: 'https://pixabay.com/photos/nachos-chips-food-mexican-plate-4454941/',
+        width: '300',
+        height: '300',
+    },
+    {
+        name: 'Feijoada',
+        description: ' black beans, beacon and sausages ',
+        country: 'Brazil',
+        imgSrc: 'https://pixabay.com/photos/bean-stew-black-beans-beans-bacon-5181831/',
+        width: '300',
+        height: '300',
+    },
 ]
 
 module.exports = intFood
