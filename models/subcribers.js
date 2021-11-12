af08bd2a8fbd5fcd81915f7fadd2ba48f51f2079
Mysia Santana <mysiasantana@gmail.com>
2021-11-12 14:19:27 -0700

    added subscribers list

---
+++ models/subcribers.js
@@ -0,0 +1,49 @@
+const subscribers = [{
+        name: 'John',
+        email: "john@iwsu.com.br",
+    },
+    {
+        name: 'Johnson',
+        email: "johnson@iwsu.com.br",
+    },
+    {
+        name: 'Jean',
+        email: "jean@iwsu.com.br",
+    },
+    {
+        name: 'Jeanne',
+        email: "jeanne@iwsu.com.br",
+    },
+    {
+        name: 'Joanne',
+        email: "joanne@iwsu.com.br",
+    },
+    {
+        name: 'Joshua',
+        email: "jonshua@iwsu.com.br",
+    },
+    {
+        name: 'Jennifer',
+        email: "jennifer@iwsu.com.br",
+    },
+    {
+        name: 'Joseph ',
+        email: "joseph@iwsu.com.br",
+    },
+    {
+        name: 'James',
+        email: "james@iwsu.com.br",
+    },
+    {
+        name: 'Jessica',
+        email: "jessica@iwsu.com.br",
+    },
+    {
+        name: 'Juan',
+        email: "juan@iwsu.com.br",
+    },
+    {
+        name: 'Jacob',
+        email: "jacob@iwsu.com.br",
+    },
+]
