969fff111562275b115cbf91740d22218b1cf653
Jean-Marc <jmarcder@gmail.com>
2021-11-13 13:21:47 -0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -2,30 +2,30 @@
 
 const intFood = [
 
-  {
-    id: 01,
-    name: '',
-    description: '',
-    imageSrc: '',
-    width: '',
-    height: ''
-  },
-  {
-    id: 02,
-    name: '',
-    description: '',
-    imageSrc: '',
-    width: '',
-    height: ''
-  },
-  {
-    id: 03,
-    name: '',
-    description: '',
-    imageSrc: '',
-    width: '',
-    height: ''
-  },
+    {
+        id: 01,
+        name: '',
+        description: '',
+        imageSrc: '',
+        width: '',
+        height: ''
+    },
+    {
+        id: 02,
+        name: '',
+        description: '',
+        imageSrc: '',
+        width: '',
+        height: ''
+    },
+    {
+        id: 03,
+        name: '',
+        description: '',
+        imageSrc: '',
+        width: '',
+        height: ''
+    },
 ]
 
 module.exports = intFood
