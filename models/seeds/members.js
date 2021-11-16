5eb582a888c17ba4f11df05a4ccdf4a3927d02ff
Jean-Marc <jmarcder@gmail.com>
2021-11-15 18:26:55 -0700

    Edited links

--- models/seeds/members.js
+++ models/seeds/members.js
@@ -1,23 +1,22 @@
-module.exports = [
-  {
-  name: 'James',
-  profilePic: '/images/avatars/James.jpg',
-  title: 'Kraft Dinner Connoisseur',
-  bio: 'I have no idea how to take a profile pic',
-  github: 'https://github.com/Archangel767'
-  },
-  {
-  name: '',
-  profilePic: '',
-  title: '',
-  bio: '',
-  github: ''
-  },
-  {
-  name: '',
-  profilePic: '',
-  title: '',
-  bio: '',
-  github: ''
-  }
-]
+module.exports = [{
+        name: 'James',
+        profilePic: '/images/avatars/James.jpg',
+        title: 'Kraft Dinner Connoisseur',
+        bio: 'I have no idea how to take a profile pic',
+        github: 'https://github.com/Archangel767'
+    },
+    {
+        name: 'Jean-Marc',
+        profilePic: '',
+        title: 'Procrastinator Level: Extreme',
+        bio: 'Why did I leave things to the last minute?',
+        github: 'https://github.com/QDetective'
+    },
+    {
+        name: '',
+        profilePic: '',
+        title: '',
+        bio: '',
+        github: ''
+    }
+];
