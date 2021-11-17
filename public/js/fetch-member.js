ceb221c54aca1196e39b55306bbe4014cb697b6b
Jean-Marc <jmarcder@gmail.com>
2021-11-17 03:23:30 -0700

    fetch json for team members from db

---
+++ public/js/fetch-member.js
@@ -0,0 +1,26 @@
+fetch('https://cpnt262-final-intfood.herokuapp.com/api/member')
+    .then(function(response) {
+        // Fetch JSON file from mongodb server to convert to JS object asynchronously.
+        if (!response.ok) {
+            throw new Error('Not 200 Ok');
+        }
+        return response.json();
+    })
+    .then(function(item) {
+        let output = ''
+        item.forEach((member) => {
+            output +=
+                `<figure>
+                <h2 class="title">${member.fullName}</h2>
+                <img src="${member.profilePic}" alt="${member.description}"></img>
+                <figcaption>
+                    <p>${member.bio}</p>
+                    <a class="author" href="${member.github}"></a>
+                </figcaption>
+            </figure>`
+        })
+
+        const team = document.querySelector('.team')
+        team.innerHTML = output
+        console.log(item)
+    })
