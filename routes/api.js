60041a93fef10bfa652228e2a05544a03765ba53
Jean-Marc <jmarcder@gmail.com>
2021-11-17 02:30:50 -0700

    Revert "creating a fetch function to pull from mongodb"
    This reverts commit ed751c51c24385e5831aa33a9486e6fd639a5862.

--- routes/api.js
+++ routes/api.js
@@ -8,151 +8,153 @@ const Image = require('../models/image');
 const Member = require('../models/member');
 const Subscriber = require('../models/subscriber');
 
-const mongoose = require(mongoose);
-
 // Image Entry route
 
 app.get('/gallery', (req, res) => {
-    let image = null
-    Image.find((err, data) => {
-        if (err) {
-            console.log(err)
-            res.sendStatus(404);
-        } else {
-            image = data
-            console.log(data)
-            if (typeof image !== 'undefined' && Array.isArray(image)) {
-                res.send(image)
-            } else {
-                res.status(404)
-                res.send({ error: 'File Not Found' })
-            }
-        }
-    })
+  let image = null
+  Image.find((err, data) => {
+    if (err) {
+      console.log(err)
+      res.sendStatus(404);
+    }
+    else {
+      image = data
+      console.log(data)
+      if (typeof image !== 'undefined' && Array.isArray(image)) {
+        res.send(image)
+      } else {
+        res.status(404)
+        res.send({ error: 'File Not Found' })
+      }
+    }
+  })
 })
 
 // Image Item Route:
 
 app.get('/gallery/:name', (req, res) => {
-    let image
-    Image.findOne({ name: req.params.name }, function(err, data) {
-        if (err) {
-            console.log(err)
-            res.sendStatus(404);
-        } else {
-            image = data
-            console.log(image)
-            if (typeof image === 'object' && image !== null) {
-                res.send(image)
-            } else {
-                res.status(404)
-                res.send({ error: 'File Not Found' })
-            }
-        }
-    });
+  let image
+  Image.findOne({ name: req.params.name }, function (err, data) {
+    if (err) {
+      console.log(err)
+      res.sendStatus(404);
+    } else {
+      image = data
+      console.log(image)
+      if (typeof image === 'object' && image !== null) {
+        res.send(image)
+      } else {
+        res.status(404)
+        res.send({ error: 'File Not Found' })
+      }
+    }
+  });
 })
 
 // Member Entry route
 
 app.get('/member', (req, res) => {
-    let member = null
-    Member.find((err, data) => {
-        if (err) {
-            console.log(err)
-            res.sendStatus(404);
-        } else {
-            member = data
-            console.log(data)
-            if (typeof member !== 'undefined' && Array.isArray(member)) {
-                res.send(member)
-            } else {
-                res.status(404)
-                res.send({ error: 'File Not Found' })
-            }
-        }
-    })
+  let member = null
+  Member.find((err, data) => {
+    if (err) {
+      console.log(err)
+      res.sendStatus(404);
+    }
+    else {
+      member = data
+      console.log(data)
+      if (typeof member !== 'undefined' && Array.isArray(member)) {
+        res.send(member)
+      } else {
+        res.status(404)
+        res.send({ error: 'File Not Found' })
+      }
+    }
+  })
 })
 
 // Member Item Route:
 
 app.get('/member/:name', (req, res) => {
-    let member
-    Member.findOne({ name: req.params.name }, function(err, data) {
-        if (err) {
-            console.log(err)
-            res.sendStatus(404);
-        } else {
-            member = data
-            console.log(member)
-            if (typeof member === 'object' && member !== null) {
-                res.send(member)
-            } else {
-                res.status(404)
-                res.send({ error: 'File Not Found' })
-            }
-        }
-    });
+  let member
+  Member.findOne({ name: req.params.name }, function (err, data) {
+    if (err) {
+      console.log(err)
+      res.sendStatus(404);
+    } else {
+      member = data
+      console.log(member)
+      if (typeof member === 'object' && member !== null) {
+        res.send(member)
+      } else {
+        res.status(404)
+        res.send({ error: 'File Not Found' })
+      }
+    }
+  });
 })
 
 // Subscriber Entry route
 
 app.get('/subscriber', (req, res) => {
-    let subscriber = null
-    Subscriber.find((err, data) => {
-        if (err) {
-            console.log(err)
-            res.sendStatus(404);
-        } else {
-            subscriber = data
-            console.log(data)
-            if (typeof subscriber !== 'undefined' && Array.isArray(subscriber)) {
-                res.send(subscriber)
-            } else {
-                res.status(404)
-                res.send({ error: 'File Not Found' })
-            }
-        }
-    })
+  let subscriber = null
+  Subscriber.find((err, data) => {
+    if (err) {
+      console.log(err)
+      res.sendStatus(404);
+    }
+    else {
+      subscriber = data
+      console.log(data)
+      if (typeof subscriber !== 'undefined' && Array.isArray(subscriber)) {
+        res.send(subscriber)
+      } else {
+        res.status(404)
+        res.send({ error: 'File Not Found' })
+      }
+    }
+  })
 })
 
 // Subscriber Item Route:
 
 app.get('/subscriber/:name', (req, res) => {
-    let subscriber
-    Subscriber.findOne({ name: req.params.name }, function(err, data) {
-        if (err) {
-            console.log(err)
-            res.sendStatus(404);
-        } else {
-            subscriber = data
-            console.log(subscriber)
-            if (typeof subscriber === 'object' && subscriber !== null) {
-                res.send(subscriber)
-            } else {
-                res.status(404)
-                res.send({ error: 'File Not Found' })
-            }
-        }
-    });
+  let subscriber
+  Subscriber.findOne({ name: req.params.name }, function (err, data) {
+    if (err) {
+      console.log(err)
+      res.sendStatus(404);
+    } else {
+      subscriber = data
+      console.log(subscriber)
+      if (typeof subscriber === 'object' && subscriber !== null) {
+        res.send(subscriber)
+      } else {
+        res.status(404)
+        res.send({ error: 'File Not Found' })
+      }
+    }
+  });
 })
 
 // 
-app.post('/subscriber', async(req, res) => {
+app.post('/subscriber', async (req, res) => {
 
-    try {
-        const subscriber = new Subscriber(req.body)
+  try {
+    const subscriber = new Subscriber(req.body)
+  
+    await subscriber.save()
+  
+    console.log(subscriber)
+    res.send('Subscriber created!')
+    
+  } catch(err) {
 
-        await subscriber.save()
-
-        console.log(subscriber)
-        res.send('Subscriber created!')
-
-    } catch (err) {
-
-        console.log(err)
-        res.send('Problem submitting form. please try again.')
-
-    }
+    console.log(err)
+    res.send('Problem submitting form. please try again.')
+    
+  }
 })
 
 module.exports = app
+
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+
 // Required modules to import
-const dotenv = require('dotenv').config()
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 
 const mongoose = require('./public/db')
 
-// Setup routes
 const routes = require('./routes/api')
 app.use('/api', routes)
 
@@ -14,26 +15,26 @@ app.use(express.static('public'))
 
 
 // Handle 404 errors with middleware
-app.use(function(req, res) => {
+app.use((req, res) => {
 
-    // If path starts with `/api`, send JSON 404
-    if (req.url.startsWith('/api')) {
+  // If path starts with `/api`, send JSON 404
+  if (req.url.startsWith('/api')) {
 
-        res.status(404)
-        res.send({ error: 'File Not Found' })
+    res.status(404)
+    res.send({error: 'File Not Found'})
 
-    } else {
+  } else {
+  
+    // else send HTML 404
+    res.status(404)
+    res.send('<h1>404: File Not Found</h1>')
 
-        // else send HTML 404
-        res.status(404)
-        res.send('<h1>404: File Not Found</h1>')
-
-    }
+  }
 });
 
 // Start server
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function() {
-    console.log(`Listening on port ${PORT}`);
+app.listen(PORT, function(){
+  console.log(`Listening on port ${PORT}`);
 });
