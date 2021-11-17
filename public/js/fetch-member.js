488d20b4014e715f6cecc6d0952ae61f38843d83
Jean-Marc <jmarcder@gmail.com>
2021-11-17 04:24:00 -0700

    update fetch-member.js

--- public/js/fetch-member.js
+++ public/js/fetch-member.js
@@ -1,18 +1,21 @@
+'use strict';
+const strict = "Strict Mode Script"
+
+let output = '';
+
 fetch('https://cpnt262-final-intfood.herokuapp.com/api/member')
     .then(function(response) {
         // Fetch JSON file from mongodb server to convert to JS object asynchronously.
         if (!response.ok) {
-            throw new Error('Not 200 Ok');
+            throw new Error('Not 200 OK');
         }
         return response.json();
     })
     .then(function(data) {
-        console.log(data)
+            console.log(data)
 
-    })
-
-for (let i = 0; i < Number(data.length); i++) {
-    output += `
+            for (let i = 0; i < Number(data.length); i++) {
+                output += `
             <div>
                 <figure>
                     <h2 class="name">${data[i].name}</h2>
@@ -23,11 +26,10 @@ for (let i = 0; i < Number(data.length); i++) {
                 </figure>
                 </div>
             `
-})
+            })
 
-console.log(output)
-document.querySelector('section').innerHTML = output;
-})
+        console.log(output) document.querySelector('section').innerHTML = output;
+    })
 .catch(function(err) {
     //console.log(err);
 
