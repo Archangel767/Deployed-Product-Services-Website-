24e50980c4fd23ba20f5b467051c862e14e4f120
Jean-Marc <jmarcder@gmail.com>
2021-11-17 04:00:14 -0700

    Cleaned up code.

--- public/js/fetch-member.js
+++ public/js/fetch-member.js
@@ -6,21 +6,25 @@ fetch('https://cpnt262-final-intfood.herokuapp.com/api/member')
         }
         return response.json();
     })
-    .then(function(item) {
-        let output = ''
-        item.forEach((member) => {
-            output +=
-                `<figure>
-                <h2 class="title">${member.name}</h2>
-                <img src="${member.profilePic}" alt="${member.title}"></img>
-                <figcaption>
-                    <p>${member.bio}</p>
-                    <a class="author" href="${member.github}"></a>
-                </figcaption>
-            </figure>`
-        })
+    .then(function(data) {
+        console.log(data)
 
-        const team = document.querySelector('.team')
-        team.innerHTML = output
-        console.log(item)
     })
+
+for (let i = 0; i < Number(data.length); i++) {
+    output += `
+        <div>
+            <figure>
+                <h2 class="name">${data[i].name}</h2>
+                <img src="${public\images\avatars\${data[i].profilePic}" alt="">$</img>
+                <figcaption><p>${data[i].bio}</p>
+                    <a href="${data[i].github}" target="_blank">Github Link</a>
+                </figcaption>
+            </figure>
+            </div>
+            `
+})
+console.log(output)
+const team = document.querySelector('.team')
+team.innerHTML = output
+})
